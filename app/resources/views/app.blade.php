e03add524c3033db0b503b853a6111ac3ed785d7
vasenin26 <leonid.vasenin@gmail.com>
2025-09-14 18:22:03 +0300

    fuck! again

--- app/resources/views/app.blade.php
+++ app/resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta name="api-base-url" content="{{ config('app.url') }}/api">
+        <meta name="api-base-url" content="{{ env('API_URL') }}">
 
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
         <script>
