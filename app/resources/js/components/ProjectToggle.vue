60605b41e01b682086fc4b51431fb5f5ba54c0b8
vasenin26 <leonid.vasenin@gmail.com>
2025-10-17 11:35:57 +0300

    Refactor ProjectToggle component to use DropdownMenu for project selection, enhancing UI interaction and removing unnecessary state management.

--- app/resources/js/components/ProjectToggle.vue
+++ app/resources/js/components/ProjectToggle.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import { useProjectStore } from '@/stores/project';
 import { Folder, LayoutGrid } from 'lucide-vue-next';
-import { computed, ref, onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import { Link } from '@inertiajs/vue3';
 
 const projectStore = useProjectStore();
-
-const showList = ref<boolean>(true);
 
 // Динамическое название и иконка
 const displayInfo = computed(() => {
@@ -26,10 +26,6 @@ const displayInfo = computed(() => {
     }
 });
 
-function showProjectList() {
-    showList.value = true;
-}
-
 onMounted(() => {
     // Загружаем список проектов при инициализации
     projectStore.loadProjects();
@@ -37,49 +33,48 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="project-toggle">
-        <div class="button">
-            <div class="button-icon" @click="showProjectList">
-                <component :is="displayInfo.icon" class="size-5 fill-current text-white dark:text-black" />
-            </div>
-            <div class="button-title">
-                {{ displayInfo.title }}
-            </div>
-            <div class="button-tip">
-                <span>
-                    <span v-if="displayInfo.isProject"> ID:{{ displayInfo.subtitle }} </span>
-                    <span v-else> Выберите проект -> </span>
-                </span>
-            </div>
-        </div>
-        <div class="list bg-card shadow-sm" v-if="showList">
-            <div v-if="projectStore.projectsLoading" class="p-2 text-sm text-muted-foreground">
-                Загрузка...
-            </div>
-            <div v-else>
-                <div
-                    class="item cursor-pointer p-2 hover:bg-accent"
-                    v-for="project in projectStore.projects"
-                    :key="project.id"
-                >
-                    {{ project.title }}
+    <DropdownMenu>
+        <DropdownMenuTrigger as-child>
+            <div class="project-toggle">
+                <div class="button">
+                    <div class="button-icon">
+                        <component :is="displayInfo.icon" class="size-5 fill-current text-white dark:text-black" />
+                    </div>
+                    <div class="button-title">
+                        {{ displayInfo.title }}
+                    </div>
+                    <div class="button-tip">
+                        <span>
+                            <span v-if="displayInfo.isProject"> ID:{{ displayInfo.subtitle }} </span>
+                            <span v-else> Выберите проект -> </span>
+                        </span>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="start" class="w-[200px] p-0">
+            <div class="bg-card shadow-sm">
+                <div v-if="projectStore.projectsLoading" class="p-2 text-sm text-muted-foreground">
+                    Загрузка...
+                </div>
+                <div v-else>
+                    <Link
+                        class="item cursor-pointer p-2 hover:bg-accent block"
+                        v-for="project in projectStore.projects"
+                        :key="project.id"
+                        :href="`/projects/${project.id}`"
+                    >
+                        {{ project.title }}
+                    </Link>
+                </div>
+            </div>
+        </DropdownMenuContent>
+    </DropdownMenu>
 </template>
 
 <style scoped lang="scss">
 .project-toggle {
     position: relative;
-}
-
-.list {
-    position: absolute;
-    padding: 10px;
-    left: 0;
-    top: 100%;
-    right: 0;
 }
 
 .button {
