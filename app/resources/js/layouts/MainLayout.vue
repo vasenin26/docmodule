d89d56c4641e3b0e085fa9918566b43bea9c5c41
vasenin26 <leonid.vasenin@gmail.com>
2025-10-31 20:59:22 +0300

    show agent task on main layout

---
+++ app/resources/js/layouts/MainLayout.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+import AgentTasksPanel from '@/components/AgentTasksPanel/AgentTasksPanel.vue';
+</script>
+
+<template>
+    <template>
+        <slot></slot>
+        <AgentTasksPanel/>
+    </template>
+</template>
+
+<style scoped>
+
+</style>
--- app/resources/js/layouts/fullscreen/FullScreenLayout.vue
+++ app/resources/js/layouts/fullscreen/FullScreenLayout.vue
@@ -1,62 +1,64 @@
 <script setup lang="ts">
-
 import ProfileInfo from '@/components/ProfileInfo.vue';
-import ProjectToggle from '@/components/ProjectToggle.vue';
 import ProjectMenu from '@/components/ProjectMenu.vue';
+import ProjectToggle from '@/components/ProjectToggle.vue';
+import MainLayout from '@/layouts/MainLayout.vue';
 </script>
 
 <template>
-    <div class="app-fullscreen">
-        <div class="header bg-card shadow-sm gap-2">
-            <div class="title">
-                <ProjectToggle/>
+    <MainLayout>
+        <div class="app-fullscreen">
+            <div class="header gap-2 bg-card shadow-sm">
+                <div class="title">
+                    <ProjectToggle />
+                </div>
+                <div class="project-menu">
+                    <ProjectMenu />
+                </div>
+                <div class="context-actions flex items-center gap-2">
+                    <slot name="context-actions" />
+                </div>
+                <div class="profile">
+                    <ProfileInfo />
+                </div>
             </div>
-            <div class="project-menu">
-                <ProjectMenu/>
-            </div>
-            <div class="context-actions flex items-center gap-2">
-                <slot name="context-actions" />
-            </div>
-            <div class="profile">
-                <ProfileInfo />
+            <div class="body">
+                <slot></slot>
             </div>
         </div>
-        <div class="body">
-            <slot></slot>
-        </div>
-    </div>
+    </MainLayout>
 </template>
 
 <style scoped lang="scss">
+$header-height: 46px;
+$main-padding: 15px;
 
-    $header-height: 46px;
-    $main-padding: 15px;
+.header {
+    height: $header-height;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+    position: fixed;
+    left: 0;
+    top: 0;
+    right: 0;
+    padding-right: $main-padding;
+    padding-left: $main-padding;
+    z-index: 100;
 
-    .header {
-        height: $header-height;
+    .profile {
+        flex-grow: 1;
         display: flex;
-        flex-wrap: wrap;
-        align-items: center;
-        justify-content: space-between;
-        position: fixed;
-        left: 0;
-        top: 0;
-        right: 0;
-        padding-right: $main-padding;
-        padding-left: $main-padding;
-        z-index: 100;
-        .profile {
-            flex-grow: 1;
-            display: flex;
-            justify-content: end;
-        }
+        justify-content: end;
     }
+}
 
-    .app-fullscreen {
-        padding-top: $header-height;
-    }
+.app-fullscreen {
+    padding-top: $header-height;
+}
 
-    .body {
-        padding: 15px;
-    }
+.body {
+    padding: 15px;
+}
 </style>
